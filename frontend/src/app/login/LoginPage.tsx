a8027ca822d696af9dd73a307a050310a90ce61f
RevolverOcelotIII <danpgaldino@gmail.com>
2026-05-24 20:06:15 -0300

    feat: adding login api request to login

--- frontend/src/app/login/LoginPage.tsx
+++ frontend/src/app/login/LoginPage.tsx
@@ -1,18 +1,27 @@
 "use client";
-
 import { useRouter } from "next/navigation";
 import { LoginSidebar } from "@/src/app/login/LoginSidebar";
 import { LoginForm } from "@/src/app/login/LoginForm";
 import { LoginFormData } from "@/src/types/app/login/LoginPage";
+import { ApiService } from "@/src/services/api";
 import "@/src/styles/app/login.css";
 
 export default function LoginPage() {
   const router = useRouter();
 
-  const handleLogin = (data: LoginFormData) => {
-    console.log("Login attempt:", data);
-    router.push("/patients");
+  const handleLogin = async (data: LoginFormData) => {
+    try {
+      console.log("Login attempt:", data);
+      const response = await ApiService.post("/auth/login", data, true);
+      console.log("Login success:", response);
+      // Success redirect
+      router.push("/patients");
+    } catch (error) {
+      console.error("Login failed:", error);
+      alert("Login failed. Please check your credentials.");
+    }
   };
+
 
   return (
     <div className="login-layout">
