8bc66618462b8f794d73bc757d8a92df4fadb383
RevolverOcelotIII <danpgaldino@gmail.com>
2026-05-24 22:07:30 -0300

    feat: Adding form inputs

---
+++ frontend/src/components/layout/Form/Form.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import { FormFieldProps, InputProps, SelectProps } from "@/src/types/components/layout/Form/Form";
+import { FaChevronDown } from "react-icons/fa";
+import "@/src/styles/components/layout/form.css";
+
+export function FormField({ label, children, width = "100", error }: FormFieldProps) {
+  return (
+    <div className={`form-field width-${width}`}>
+      <label className="form-label">{label}</label>
+      <div className="form-input-container">
+        {children}
+      </div>
+      {error && <span className="form-error">{error}</span>}
+    </div>
+  );
+}
+
+export function Input({ width, ...props }: InputProps) {
+  return (
+    <input className="form-input" {...props} />
+  );
+}
+
+export function Select({ width, options, ...props }: SelectProps) {
+  return (
+    <div className="form-input-container">
+      <select className="form-select" {...props}>
+        {options.map((option) => (
+          <option key={option.value} value={option.value}>
+            {option.label}
+          </option>
+        ))}
+      </select>
+      <FaChevronDown className="select-icon" size={16} />
+    </div>
+  );
+}
