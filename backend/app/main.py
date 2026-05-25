71b63ffceb614d40dbdb193d8bf609ad28a6be58
Daniel Bahiense <47671687+RevolverOcelotIII@users.noreply.github.com>
2026-05-24 22:15:42 -0300

    feat: Adding CORS validation to the API (#15)

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,22 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.controllers import users, patients, medications, procedures, medical_records, employees, auth
 
 app = FastAPI(title="MedManager API")
+
+front_url = os.getenv("FRONT_URL", "http://localhost:3000")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[front_url],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(auth.router)
 app.include_router(users.router)
