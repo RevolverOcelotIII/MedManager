5f42d7adcf300b21d744ebf195059bd251070981
Daniel Bahiense <47671687+RevolverOcelotIII@users.noreply.github.com>
2026-05-26 22:34:39 -0300

    fix: Enhancing employees validations (#45)

--- backend/app/controllers/employees.py
+++ backend/app/controllers/employees.py
@@ -13,10 +13,16 @@ router = APIRouter(prefix="/employees", tags=["Employees"], dependencies=[Depend
 @router.get("/", response_model=List[Union[EmployeeResponse, RestrictedEmployeeResponse]])
 def list_employees(
     can_execute_procedure_id: Optional[int] = None,
+    can_dispatch_procedure_id: Optional[int] = None,
     current_user: User = Depends(get_current_user), 
     db_session: Session = Depends(get_db)
 ):
-    return EmployeeService.get_all(db_session, current_user, can_execute_procedure_id=can_execute_procedure_id)
+    return EmployeeService.get_all(
+        db_session, 
+        current_user, 
+        can_execute_procedure_id=can_execute_procedure_id,
+        can_dispatch_procedure_id=can_dispatch_procedure_id
+    )
 
 @router.get("/{employee_id}", response_model=Union[EmployeeResponse, RestrictedEmployeeResponse])
 def get_employee(employee_id: int, current_user: User = Depends(get_current_user), db_session: Session = Depends(get_db)):
